ab6eea68e425a6dcbf82e502bb74bbfbd831e1f2
Y-ono0602 <ono.yuuki.ono@gmail.com>
2019-11-08 11:52:10 +0900

    cssの崩れの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,28 +2,39 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources :items, only: [:index, :new, :create, :show]
-  resources :users do
-    member do
-      get 'mypage'
-      get 'mypage/notification'
-      get 'mypage/todo'
-      get 'mypage/like/history'
-      get 'mypage/listings/listing'
-      get 'mypage/listings/in_progress'
-      get 'mypage/listings/completed'
-      get 'mypage/purchase'
-      get 'mypage/purchased'
-      get 'mypage/news'
-      get 'mypage/review/history'
-      get 'mypage/support'
-      get 'mypage/sales'
-      get 'mypage/point'
-      get 'mypage/profile'
-      get 'mypage/deliver_address'
-      get 'mypage/card'
-      get 'mypage/email_password'
-      get 'mypage/identification'
-      get 'mypage/sms_confirmation'
-    end
-  end
+  get 'users/mypage'
+  get 'users/identification'
+  get 'users/profile'
+  get 'users/logout'
+  get 'users/new'
+  get 'users/new0'
+  get 'users/new1'
+  get 'users/new2'
+  get 'users/new3'
+  get 'users/new4'
+  get 'users/new5'
+  # resources :users do
+  #   member do
+  #     get 'mypage'
+  #     get 'mypage/notification'
+  #     get 'mypage/todo'
+  #     get 'mypage/like/history'
+  #     get 'mypage/listings/listing'
+  #     get 'mypage/listings/in_progress'
+  #     get 'mypage/listings/completed'
+  #     get 'mypage/purchase'
+  #     get 'mypage/purchased'
+  #     get 'mypage/news'
+  #     get 'mypage/review/history'
+  #     get 'mypage/support'
+  #     get 'mypage/sales'
+  #     get 'mypage/point'
+  #     get 'mypage/profile'
+  #     get 'mypage/deliver_address'
+  #     get 'mypage/card'
+  #     get 'mypage/email_password'
+  #     get 'mypage/identification'
+  #     get 'mypage/sms_confirmation'
+  #   end
+  # end
 end
