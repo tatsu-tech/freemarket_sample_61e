93def099dd0134fa0304dd96b0fee45305c0fef9
tatsu-tech <55968601+tatsu-tech@users.noreply.github.com>
2019-11-11 19:13:51 +0900

    出品商品情報のフロントへの反映

--- app/assets/javascripts/items-show.js
+++ app/assets/javascripts/items-show.js
@@ -1,62 +1,46 @@
 $(document).on('turbolinks:load', function() {
 
-    $('.top-box__detail--left--sub-photo--s').on("mouseenter", ".s-imageT", function() {
-      $(this).css("opacity", "1");
-      $('.top-box__detail--left--main-photo--list').animate({'left': '0px'}, 700);
-      $(".s-image1 , .s-image2 , .s-image3, .s-image4, .s-image5, .s-image6, .s-image7, .s-image8, .s-image9").css("opacity", "0.4");
-    })
+  $("ul.top-box__detail--left--main-photo li:eq(0)").css("left", "0");
+  $("ul.top-box__detail--left--main-photo li:eq(1)").css("left", "300px");
+  $("ul.top-box__detail--left--main-photo li:eq(2)").css("left", "600px");
+  $("ul.top-box__detail--left--main-photo li:eq(3)").css("left", "900px");
+  $("ul.top-box__detail--left--main-photo li:eq(4)").css("left", "1200px");
+  $("ul.top-box__detail--left--main-photo li:eq(5)").css("left", "1500px");
+  $("ul.top-box__detail--left--main-photo li:eq(6)").css("left", "1800px");
+  $("ul.top-box__detail--left--main-photo li:eq(7)").css("left", "2100px");
+  $("ul.top-box__detail--left--main-photo li:eq(8)").css("left", "2400px");
+  $("ul.top-box__detail--left--main-photo li:eq(9)").css("left", "2700px");
 
-    $('.top-box__detail--left--sub-photo--s').on("mouseenter", ".s-image1", function() {
-      $(this).css("opacity", "1");
-      $('.top-box__detail--left--main-photo--list').animate({'left': '-300px'}, 700);
-      $(".s-imageT , .s-image2 , .s-image3, .s-image4, .s-image5, .s-image6, .s-image7, .s-image8, .s-image9").css("opacity", "0.4");
-    })
 
-    $('.top-box__detail--left--sub-photo--s').on("mouseenter", ".s-image2", function() {
-      $(this).css("opacity", "1");
-      $('.top-box__detail--left--main-photo--list').animate({'left': '-600px'}, 700);
-      $(".s-imageT , .s-image1 , .s-image3, .s-image4, .s-image5, .s-image6, .s-image7, .s-image8, .s-image9").css("opacity", "0.4");
-    })
+  $('.top-box__detail--left--sub-photo').on("mouseenter", "li:eq(0)", function() {
+    $('.top-box__detail--left--main-photo').animate({'left': '0px'}, 550);
+  })
+  $('.top-box__detail--left--sub-photo').on("mouseenter", "li:eq(1)", function() {
+    $('.top-box__detail--left--main-photo').animate({'left': '-300px'}, 550);
+  })
+  $('.top-box__detail--left--sub-photo').on("mouseenter", "li:eq(2)", function() {
+    $('.top-box__detail--left--main-photo').animate({'left': '-600px'}, 550);
+  })
+  $('.top-box__detail--left--sub-photo').on("mouseenter", "li:eq(3)", function() {
+    $('.top-box__detail--left--main-photo').animate({'left': '-900px'}, 550);
+  })
+  $('.top-box__detail--left--sub-photo').on("mouseenter", "li:eq(4)", function() {
+    $('.top-box__detail--left--main-photo').animate({'left': '-1200px'}, 550);
+  })
+  $('.top-box__detail--left--sub-photo').on("mouseenter", "li:eq(5)", function() {
+    $('.top-box__detail--left--main-photo').animate({'left': '-1500px'}, 550);
+  })
+  $('.top-box__detail--left--sub-photo').on("mouseenter", "li:eq(6)", function() {
+    $('.top-box__detail--left--main-photo').animate({'left': '-1800px'}, 550);
+  })
+  $('.top-box__detail--left--sub-photo').on("mouseenter", "li:eq(7)", function() {
+    $('.top-box__detail--left--main-photo').animate({'left': '-2100px'}, 550);
+  })
+  $('.top-box__detail--left--sub-photo').on("mouseenter", "li:eq(8)", function() {
+    $('.top-box__detail--left--main-photo').animate({'left': '-2400px'}, 550);
+  })
+  $('.top-box__detail--left--sub-photo').on("mouseenter", "li:eq(9)", function() {
+    $('.top-box__detail--left--main-photo').animate({'left': '-2700px'}, 550);
+  })
 
-    $('.top-box__detail--left--sub-photo--s').on("mouseenter", ".s-image3", function() {
-      $(this).css("opacity", "1");
-      $('.top-box__detail--left--main-photo--list').animate({'left': '-900px'}, 700);
-      $(".s-imageT , .s-image1 , .s-image2, .s-image4, .s-image5, .s-image6, .s-image7, .s-image8, .s-image9").css("opacity", "0.4");
-    })
-
-    $('.top-box__detail--left--sub-photo--s').on("mouseenter", ".s-image4", function() {
-      $(this).css("opacity", "1");
-      $('.top-box__detail--left--main-photo--list').animate({'left': '-1200px'}, 700);
-      $(".s-imageT , .s-image1 , .s-image2, .s-image3, .s-image5, .s-image6, .s-image7, .s-image8, .s-image9").css("opacity", "0.4");
-    })
-    
-    $('.top-box__detail--left--sub-photo--s').on("mouseenter", ".s-image5", function() {
-      $(this).css("opacity", "1");
-      $('.top-box__detail--left--main-photo--list').animate({'left': '-1500px'}, 700);
-      $(".s-imageT , .s-image1 , .s-image2, .s-image3, .s-image4, .s-image6, .s-image7, .s-image8, .s-image9").css("opacity", "0.4");
-    })
-
-    $('.top-box__detail--left--sub-photo--s').on("mouseenter", ".s-image6", function() {
-      $(this).css("opacity", "1");
-      $('.top-box__detail--left--main-photo--list').animate({'left': '-1800px'}, 700);
-      $(".s-imageT , .s-image1 , .s-image2, .s-image3, .s-image4, .s-image5, .s-image7, .s-image8, .s-image9").css("opacity", "0.4");
-    })
-
-    $('.top-box__detail--left--sub-photo--s').on("mouseenter", ".s-image7", function() {
-      $(this).css("opacity", "1");
-      $('.top-box__detail--left--main-photo--list').animate({'left': '-2100px'}, 700);
-      $(".s-imageT , .s-image1 , .s-image2, .s-image3, .s-image4, .s-image5, .s-image6, .s-image8, .s-image9").css("opacity", "0.4");
-    })
-
-    $('.top-box__detail--left--sub-photo--s').on("mouseenter", ".s-image8", function() {
-      $(this).css("opacity", "1");
-      $('.top-box__detail--left--main-photo--list').animate({'left': '-2400px'}, 700);
-      $(".s-imageT , .s-image1 , .s-image2, .s-image3, .s-image4, .s-image5, .s-image6, .s-image7, .s-image9").css("opacity", "0.4");
-    })
-
-    $('.top-box__detail--left--sub-photo--s').on("mouseenter", ".s-image9", function() {
-      $(this).css("opacity", "1");
-      $('.top-box__detail--left--main-photo--list').animate({'left': '-2700px'}, 700);
-      $(".s-imageT , .s-image1 , .s-image2, .s-image3, .s-image4, .s-image5, .s-image6, .s-image7, .s-image8").css("opacity", "0.4");
-    }) 
-  });
+});
