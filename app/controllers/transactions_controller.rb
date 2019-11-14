ca4716a69e55d94726105e2cd67d5c2fcbfd74df
hakryun <hangnyeon@gmail.com>
2019-11-14 21:05:27 +0900

    5th revision

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,6 +2,7 @@ class TransactionsController < ApplicationController
   def create
     Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
     charge = Payjp::Charge.create(
+# 以下は、追って購入確認画面から引いてくる形で実装する予定。その段階では、当然顧客IDはここには記載されない。
       amount: 1000,
       customer: 'cus_12f7c6314638b6a5a02f779c71ed',
       currency: 'jpy'
