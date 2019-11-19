3d467e6afd74fe33743f17c5cc245099c635bfea
hakryun <hangnyeon@gmail.com>
2019-11-19 14:07:51 +0900

    revised

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,14 +3,12 @@ class TransactionsController < ApplicationController
 
   def new
     @item = Item.find(params[:item_id])
-    # @transaction = Transaction.new
   end
 
   def create
     @item = Item.find(params[:item_id])
     Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
     charge = Payjp::Charge.create(
-# 以下は、追って購入確認画面から引いてくる形で実装する予定。その段階では、当然顧客IDはここには記載されない。
       amount: @item.price,
       customer: Card.find_by(user_id: current_user.id).customer_id,
       currency: 'jpy'
