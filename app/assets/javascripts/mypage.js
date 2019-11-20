301682551c525383d8963dbb548e6bb1020e99c2
tatsu-tech <55968601+tatsu-tech@users.noreply.github.com>
2019-11-20 13:56:16 +0900

    マイページの修正およびトップページのランダム表示機能の追加

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -4,7 +4,7 @@ $(document).on('turbolinks:load', function() {
 
   // マイページのパスの時
   $(function() {
-    if (pathName == ("/users/1/users/mypage")) {
+    if (pathName.includes("/users/mypage")) {
       $("ul.mypage-nav__list li:eq(0) .arrow-right").css("color", "black");
       $("ul.mypage-nav__list li:eq(0)").css("background-color", "rgb(238, 238, 238)");
     
@@ -14,27 +14,27 @@ $(document).on('turbolinks:load', function() {
       $("ul.mypage-nav__list li:eq(3) .arrow-right").css("color", "black");
 
   // 出品した商品-出品中のパスの時
-    } else if (pathName == ("/users/1/users/listing")) {
+    } else if (pathName.includes("/users/listing")) {
       $("ul.mypage-nav__list li:eq(5)").css("background-color", "rgb(238, 238, 238)");
       $("ul.mypage-nav__list li:eq(5) .arrow-right").css("color", "black");
 
   // 出品した商品-取引中のパスの時
-    } else if (pathName == ("/users/1/users/in_progress")) {
+    } else if (pathName.includes("/users/in_progress")) {
       $("ul.mypage-nav__list li:eq(6)").css("background-color", "rgb(238, 238, 238)");
       $("ul.mypage-nav__list li:eq(6) .arrow-right").css("color", "black");
 
   // 出品した商品-売却済みのパスの時
-    } else if (pathName == ("/users/1/users/completed")) {
+    } else if (pathName.includes("/users/completed")) {
       $("ul.mypage-nav__list li:eq(7)").css("background-color", "rgb(238, 238, 238)");
       $("ul.mypage-nav__list li:eq(7) .arrow-right").css("color", "black");
 
   // 購入した商品-取引中のパスの時
-    } else if (pathName == ("/users/1/users/purchase")) {
+    } else if (pathName.includes("/users/purchase")) {
       $("ul.mypage-nav__list li:eq(8)").css("background-color", "rgb(238, 238, 238)");
       $("ul.mypage-nav__list li:eq(8) .arrow-right").css("color", "black");
 
   // 購入した商品-過去の取引のパスの時
-    } else if (pathName == ("/users/1/users/purchased")) {
+    } else if (pathName.includes("/users/purchased")) {
       $("ul.mypage-nav__list li:eq(9)").css("background-color", "rgb(238, 238, 238)");
       $("ul.mypage-nav__list li:eq(9) .arrow-right").css("color", "black");
 
