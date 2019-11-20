7b7a33bc331a48ad084d8c66d0bd65995fb9726e
junya30st <ji06140210@gmail.com>
2019-11-20 20:38:22 +0900

    edit-view&retouch-sns-signup

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,15 +1,16 @@
 class SignupController < ApplicationController
 
-  before_action :validates_signup1, only: :signup2         # signup2へ遷移する前にsignup1の入力内容にバリデーション
-  before_action :validates_signup2, only: :signup3         # signup3へ遷移する前にsignup2の入力内容にバリデーション
-  before_action :validates_signup3, only: :signup4         # signup4へ遷移する前にsignup3の入力内容にバリデーション
+  # before_action :validates_signup1, only: :signup2         # signup2へ遷移する前にsignup1の入力内容にバリデーション
+  # before_action :validates_signup2, only: :signup3         # signup3へ遷移する前にsignup2の入力内容にバリデーション
+  # before_action :validates_signup3, only: :signup4         # signup4へ遷移する前にsignup3の入力内容にバリデーション
 
   def index
   end
 
   def signup1                                              #サインインしている場合はサインアップ画面へ進ませないよう設定。商品一覧画面へ。
-    redirect_to root_path if user_signed_in? 
-    @user = User.new
+    redirect_to root_path if user_signed_in?
+    @user = User.new(nickname: params[:nickname],email: params[:email])
+    session[:user] = @user
   end
 
   def signup2                                              #signup1での入力内容をsessionで保持
@@ -22,68 +23,54 @@ class SignupController < ApplicationController
     session[:name_first_kanji] = user_params[:name_first_kanji]
     session[:name_family_kana] = user_params[:name_family_kana]
     session[:name_first_kana] = user_params[:name_first_kana]
-    @user = User.new
-    @user.user_address = UserAddress.new
   end
 
   def signup3                                              #signup2の入力内容をsessionで保持
-    session[:mobile_phone_number] = user_params[:mobile_phone_number]
-    @user = User.new
-    @user.user_address = UserAddress.new
+    session[:mobile_phone_number] = params[:mobile_phone_number]
+    if session[:password] == nil
+      password = Devise.friendly_token.first(7)
+      user = User.new(                                      #保持してきたUser内容を保存
+        nickname: session[:nickname],
+        email: session[:email],
+        password: password,
+        password_confirmation: password,
+        birthday: session[:birthday],
+        mobile_phone_number: session[:mobile_phone_number],
+        name_family_kanji: session[:name_family_kanji],
+        name_first_kanji: session[:name_first_kanji],
+        name_family_kana: session[:name_family_kana],
+        name_first_kana: session[:name_first_kana]
+      )
+    else
+      user = User.new(                                      #保持してきたUser内容を保存
+        nickname: session[:nickname],
+        email: session[:email],
+        password: session[:password],
+        password_confirmation: session[:password_confirmation],
+        birthday: session[:birthday],
+        mobile_phone_number: session[:mobile_phone_number],
+        name_family_kanji: session[:name_family_kanji],
+        name_first_kanji: session[:name_first_kanji],
+        name_family_kana: session[:name_family_kana],
+        name_first_kana: session[:name_first_kana]
+      )
+    end
+
+      if user.save
+        sign_in User.find(user.id) unless user_signed_in?  #保存できたらサインインする→UserAddressにuser_idを入れられる
+      else
+        redirect_to signup_signup_index_path                #保存できなかったら最初の画面に戻る
+      end
   end
 
   def signup4 
     password = create_password
     gon.pk_key = ENV['PAYJP_TEST_PUBLIC_KEY']
-    session[:send_name_family_kanji] = user_address_params[:send_name_family_kanji]
-    session[:send_name_first_kanji] = user_address_params[:send_name_first_kanji]
-    session[:send_name_family_kana] = user_address_params[:send_name_family_kana]
-    session[:send_name_first_kana] = user_address_params[:send_name_first_kana]
-    session[:zip] = user_address_params[:zip]
-    session[:prefecture] = user_address_params[:prefecture]
-    session[:city] = user_address_params[:city]
-    session[:town] = user_address_params[:town]
-    session[:apartment] = user_address_params[:apartment]
-    session[:phone_number] = user_address_params[:phone_number]
-    @user.user_address = UserAddress.new
-
-    @user = User.new(                                      #保持してきたUser内容を保存
-      nickname: session[:nickname],
-      email: session[:email],
-      password: password,
-      password_confirmation: password,
-      birthday: session[:birthday],
-      mobile_phone_number: session[:mobile_phone_number],
-      name_family_kanji: session[:name_family_kanji],
-      name_first_kanji: session[:name_first_kanji],
-      name_family_kana: session[:name_family_kana],
-      name_first_kana: session[:name_first_kana]
-      )
-      if @user.save
-        sign_in User.find(@user.id) unless user_signed_in?  #保存できたらサインインする→UserAddressにuser_idを入れられる
-      else
-        redirect_to signup_signup_index_path                #保存できなかったら最初の画面に戻る
-      end
-      # session[:id] = @user.id
-      
-      @user.user_address = UserAddress.new(                 #保持してきたUserAddress内容を保存する
-      send_name_family_kanji: session[:send_name_family_kanji],
-      send_name_first_kanji: session[:send_name_first_kanji],
-      send_name_family_kana: session[:send_name_family_kana],
-      send_name_first_kana: session[:send_name_first_kana],
-      zip: session[:zip],
-      prefecture: session[:prefecture],
-      city: session[:city],
-      town: session[:town],
-      apartment: session[:apartment],
-      phone_number: session[:phone_number]
-      )
-      
-      @user.user_address.save
+    @user_address = UserAddress.create(user_address_params)#保持してきたUserAddress内容を保存する
   end
 
-  def validates_signup1                                     #signup1での入力内容へのバリデーション
-    password = create_password
+  def validates_signup1
+    password = Devise.friendly_token.first(7)                                     #signup1での入力内容へのバリデーション
       session[:nickname] = user_params[:nickname]
       session[:email] = user_params[:email]
       session[:password] =user_params[:password]
@@ -99,7 +86,6 @@ class SignupController < ApplicationController
         password: password,
         password_confirmation: password,
         birthday: session[:birthday],
-        mobile_phone_number: "01234567890",                 #signup2でバリデーションをかけるためにダミー情報で一旦バリデーションを突破
         name_family_kanji: session[:name_family_kanji],
         name_first_kanji: session[:name_first_kanji],
         name_family_kana: session[:name_family_kana],
@@ -109,7 +95,7 @@ class SignupController < ApplicationController
   end
 
   def validates_signup2                                     #signup2での入力内容へのバリデーション
-    password = create_password
+    password = Devise.friendly_token.first(7)
     session[:mobile_phone_number] = user_params[:mobile_phone_number]
     @user = User.new(
       nickname: session[:nickname],
@@ -127,30 +113,6 @@ class SignupController < ApplicationController
   end
 
   def validates_signup3                                     #signup3での入力内容へのバリデーション
-    password = create_password
-    session[:send_name_family_kanji] = user_address_params[:send_name_family_kanji]
-    session[:send_name_first_kanji] = user_address_params[:send_name_first_kanji]
-    session[:send_name_family_kana] = user_address_params[:send_name_family_kana]
-    session[:send_name_first_kana] = user_address_params[:send_name_first_kana]
-    session[:zip] = user_address_params[:zip]
-    session[:prefecture] = user_address_params[:prefecture]
-    session[:city] = user_address_params[:city]
-    session[:town] = user_address_params[:town]
-    # session[:apartment] = user_address_params[:apartment]
-    session[:phone_number] = user_address_params[:phone_number]
-    
-    @user = User.new(
-      nickname: session[:nickname],
-      email: session[:email],
-      password: password,
-      password_confirmation: password,
-      birthday: session[:birthday],
-      mobile_phone_number: session[:mobile_phone_number],
-      name_family_kanji: session[:name_family_kanji],
-      name_first_kanji: session[:name_first_kanji],
-      name_family_kana: session[:name_family_kana],
-      name_first_kana: session[:name_first_kana]
-      )
     @user.user_address = UserAddress.new(
       send_name_family_kanji: session[:send_name_family_kanji],
       send_name_first_kanji: session[:send_name_first_kanji],
@@ -160,7 +122,7 @@ class SignupController < ApplicationController
       prefecture: session[:prefecture],
       city: session[:city],
       town: session[:town],
-      # apartment: session[:apartment],
+      apartment: session[:apartment],
       phone_number: session[:phone_number]
       )
       render '/signup/signup3' unless @user.user_address.valid? #バリデーションにかかると画面は遷移せずもう一度入力するよう促す
@@ -184,7 +146,7 @@ class SignupController < ApplicationController
   end
 
   def user_address_params
-    params.require(:user_address).permit(
+    params.permit(
       :send_name_family_kanji,
       :send_name_first_kanji,
       :send_name_family_kana,
@@ -195,7 +157,7 @@ class SignupController < ApplicationController
       :town,
       # :apartment,
       :phone_number
-    )
+    ).merge(user_id: current_user.id)
   end
 
   def create_password                                   #パスワードが入力された場合は、そのパスワードを。入力されていない場合は自動生成し、返り値として返す。
